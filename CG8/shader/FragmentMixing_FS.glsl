18b67f2c3782cf82426b25c2f9410f239b7230a2
Kevin <moogle.seidel@gmail.com>
2012-06-14 17:27:26 +0200

    Added FragmentMixing Vertex and Fragment Shader Not working!

---
+++ CG8/shader/FragmentMixing_FS.glsl
@@ -0,0 +1,14 @@
+#version 150 core
+
+uniform vec3 inverseLightDirection;
+
+in vec3 normalWC;
+in vec4 fragmentColor1;
+in vec4 fragementColor2;
+
+out vec4 fragmentColor;
+
+void main(void) {
+    mix();
+
+}
---
+++ CG8/shader/FragmentMixing_VS.glsl
@@ -0,0 +1,22 @@
+#version 150 core
+
+uniform mat4 viewProj;
+uniform mat4 model;
+uniform mat4 modelIT; 
+
+in vec3 modelMC;
+in vec3 normalMC;
+in vec4 vertexColor:
+in vec4 vertexColor2;
+
+out vec3 normalWC;
+out vec4 fragmentColor1;
+out vec4 fragementColor2;
+
+void main(void) {
+    gl_Position = viewProj * model * vec4(modelMC, 1);
+    normalWC = viewProj * model * vec4(nromalMC, 1);
+    fragmentColor1 = vertexColor;
+    fragmentColor2 = vertexColor2;
+    
+}
